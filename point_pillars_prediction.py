9c377341fd8d6f95330b1fc29beba283afd1d3f7
Anjul Tyagi <anjul.ten@gmail.com>
2020-08-11 12:45:03 +0000

    Corrected the inference file

--- point_pillars_prediction.py
+++ point_pillars_prediction.py
@@ -2,32 +2,33 @@ import os
 import tensorflow as tf
 from glob import glob
 from processors import SimpleDataGenerator
-from readers import LivoxDataReader
+from readers import KittiDataReader
 from config import Parameters
 from network import build_point_pillar_graph
 
-DATA_ROOT = "/media/nicholas/rkancharla/LiDAR/data/Livox/eval/"  
+DATA_ROOT = "../validation_small"  
 MODEL_ROOT = "./logs"
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 
 if __name__ == "__main__":
 
     params = Parameters()
-    pillar_net = build_point_pillar_graph(params, is_training=False)
+    pillar_net = build_point_pillar_graph(params)
     pillar_net.load_weights(os.path.join(MODEL_ROOT, "model.h5"))
     pillar_net.summary()
 
-    data_reader = LivoxDataReader()
+    data_reader = KittiDataReader()
 
-    lidar_files = sorted(glob(os.path.join(DATA_ROOT, "points", "*.bin")))
-    label_files = sorted(glob(os.path.join(DATA_ROOT, "labels", "*.pkl")))
+    lidar_files = sorted(glob(os.path.join(DATA_ROOT, "velodyne", "*.bin")))
+    label_files = sorted(glob(os.path.join(DATA_ROOT, "label_2", "*.txt")))
+    calibration_files = sorted(glob(os.path.join(DATA_ROOT, "calib", "*.txt")))
 
-    eval_gen = SimpleDataGenerator(data_reader, params.eval_batch_size, lidar_files)
+    eval_gen = SimpleDataGenerator(data_reader, params.batch_size, lidar_files, label_files, calibration_files)
 
-    pred = pillar_net.predict(eval_gen, batch_size=1)
+    pred = pillar_net.predict(eval_gen)
 
     print(pred)
     
